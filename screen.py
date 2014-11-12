0df0ac76c296466fa438ebb488b543b7207588da
Aubort Jean-Baptiste <jean-baptiste.aubort@epfl.ch>
2014-11-12 12:36:34 +0100

    prepare for recurrence handling

--- screen.py
+++ screen.py
@@ -38,43 +38,54 @@ app.secret_key = cfg.secret_key
 # Functions
 #
 
+def get_recurrences(dt_start, dt_end, freq, byday):
+    pass
+
+def get_event(calendar, summary, location, description, dt_start, dt_end):
+    return {
+        'cal': calendar,
+        'name': summary,
+        's_day': dt_start.dt.strftime('%d'),
+        's_month': dt_start.dt.strftime('%b'),
+        's_year': dt_start.dt.strftime('%Y'),
+        's_time': dt_start.dt.strftime('%H:%M'),
+        'e_day': dt_end.dt.strftime('%d'),
+        'e_month': dt_end.dt.strftime('%b'),
+        'e_year': dt_end.dt.strftime('%Y'),
+        'e_time': dt_end.dt.strftime('%H:%M'),
+        'timestamp': arrow.Arrow.fromdate(dt_start.dt).timestamp,
+        'location': location,
+        'description': description,
+    }
+
 def get_calendar(url):
     name = ''
     events = []
     cal_data = requests.get(url, headers={'User-Agent': cfg.user_agent}).content
     try:
         cal_obj = icalendar.Calendar.from_ical(cal_data)
-        name = cal_obj.get('x-wr-calname')
+        cal_name = cal_obj.get('x-wr-calname')
         yesterday = arrow.now().floor('day').replace(days=-1)
         for e in cal_obj.walk():
             if e.name != 'VEVENT':
                 continue
-            summary = e.get('summary')
+            # Only future or recent events
             date_start = e.get('dtstart')
-            date_end = e.get('dtend')
-            location = e.get('location')
-            desc = e.get('description')
             if arrow.Arrow.fromdate(date_start.dt) < yesterday:
                 continue
-            evt = {
-                'cal': name,
-                'name': summary,
-                's_day': date_start.dt.strftime('%d'),
-                's_month': date_start.dt.strftime('%b'),
-                's_year': date_start.dt.strftime('%Y'),
-                's_time': date_start.dt.strftime('%H:%M'),
-                'e_day': date_end.dt.strftime('%d'),
-                'e_month': date_end.dt.strftime('%b'),
-                'e_year': date_end.dt.strftime('%Y'),
-                'e_time': date_end.dt.strftime('%H:%M'),
-                'timestamp': arrow.Arrow.fromdate(date_start.dt).timestamp,
-                'location': location,
-                'description': desc,
-            }
-            if evt not in events: # Some events appear twice
+            #TODO Handle recurence
+            if e.get('rrule') != None:
+                continue
+            summary = e.get('summary')
+            location = e.get('location')
+            desc = e.get('description')
+            date_end = e.get('dtend')
+            # Create and add event
+            evt = get_event(cal_name, summary, location, desc, date_start, date_end)
+            if evt not in events:
                 events.append(evt)
     except IOError, e:
-        events.append({'name': e, 'cal': name})
+        events.append({'name': e, 'cal': cal_name})
     return events
 
 #
