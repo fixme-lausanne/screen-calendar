a9152dad8dae8162e1cfbf21951051f3d154f306
Aubort Jean-Baptiste <jean-baptiste.aubort@epfl.ch>
2014-11-20 13:25:10 +0100

    Add UC events back

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ secret_key = 'asdasd' #Change me !
 calendars = [
     'https://www.google.com/calendar/ical/sruulkb8vh28dim9bcth8emdm4%40group.calendar.google.com/public/basic.ics',
     'https://www.google.com/calendar/ical/mgvq9goephpmna0kb7rj9ipei8%40group.calendar.google.com/public/basic.ics',
-#    'https://www.google.com/calendar/ical/eru0rpe6q4ra6l966v11pcp1a0%40group.calendar.google.com/public/basic.ics',
+    'https://www.google.com/calendar/ical/eru0rpe6q4ra6l966v11pcp1a0%40group.calendar.google.com/public/basic.ics',
 ]
 
