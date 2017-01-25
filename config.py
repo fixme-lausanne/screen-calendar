b8b46a4e589d9d3a32af098da63aa5c81668c164
Jean-Baptiste Aubort <jean-baptiste.aubort@epfl.ch>
2017-01-25 11:54:21 +0100

    Add fablab renens

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ user_agent = 'fixme-calendar/0.1'
 secret_key = 'asdasd' #Change me !
 calendars = [
     'https://www.google.com/calendar/ical/sruulkb8vh28dim9bcth8emdm4%40group.calendar.google.com/public/basic.ics',
-    'https://www.google.com/calendar/ical/mgvq9goephpmna0kb7rj9ipei8%40group.calendar.google.com/public/basic.ics',
-    'https://www.google.com/calendar/ical/eru0rpe6q4ra6l966v11pcp1a0%40group.calendar.google.com/public/basic.ics',
+    'https://calendar.google.com/calendar/ical/fablab.renens%40gmail.com/public/basic.ics',
 ]
 
